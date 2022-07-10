15ff41cf3a72e553bb56aa1487b80073792aaf12
Victor <victorgpj25@gmail.com>
2022-07-10 13:40:18 -0300

    Cart + Files organization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+
+import UserContext from "./contexts/UserContext.js"
+import GlobalStyle from "./styles/GlobalStyles.js"
+import Home from "./pages/Home.js"
+import User from "./pages/User.js"
+import SignIn from "./pages/SignIn.js"
+import SignUp from "./pages/SignUp.js"
+import Cart from "./pages/Cart.js"
+
 
 
 
 export default function App () {
 
+    const [ config, setConfig ] = useState({headers: {Authorization: `Bearer ${localStorage.getItem("config")}`}} || "")
+    const [ loading, setLoading ] = useState(false)
+
     return (
-                <Routes>
-                    <Route path="/" element={<></>} />
-            
-                </Routes>
+        <UserContext.Provider value={{ config, setConfig, loading, setLoading }}>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<Cart />} />
+                <Route path="/minha-conta" element={<User />} />
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/cadastro" element={<SignUp />} />
+                <Route path="/carrinho" element={<Home />} />
+            </Routes>
+        </UserContext.Provider>
     )
 }
---
+++ src/components/home/Product.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default function Product () {
+    return (
+        <>
+            <Container>
+                <h1>IN DEVELOPMENT</h1>
+            </Container> 
+        </>
+    )
+}
+const Container = styled.div`
+
+    h1 {
+        font-family: 'VT323';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 58px;
+        line-height: 58px;
+
+        color: #000000;
+    }
+`;
